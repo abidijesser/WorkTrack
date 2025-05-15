3b9323192c572f6855943001cc5e55620cf1182a
abidijesser <jasserabidi00@gmail.com>
2025-05-15 21:54:27 +0200

    Updates

---
+++ assets/CCardGroup-BZjGkYKK.js
@@ -0,0 +1,1 @@
+import{r as p,_ as t,R as d,E as n,G as i,P as r}from"./index-D6BHh2cn.js";var s=p.forwardRef(function(a,e){var c=a.children,l=a.className,o=t(a,["children","className"]);return d.createElement("div",n({className:i("card-group",l)},o,{ref:e}),c)});s.propTypes={children:r.node,className:r.string};s.displayName="CCardGroup";export{s as C};
